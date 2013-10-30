14bbdc2e3d92b1038bc8f49ab20847948830ebd0
nicashmu <nicashmu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-30 13:54:45 +0000

    update to 3.7.1
    git-svn-id: https://plugins.svn.wordpress.org/video-playlist-and-gallery-plugin/trunk@795929 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-media-cincopa.php
+++ wp-media-cincopa.php
@@ -4,12 +4,12 @@ Plugin Name: Post video players, slideshow albums, photo galleries and music / p
 Plugin URI: http://www.cincopa.com/media-platform/wordpress-plugin.aspx
 Description: Post rich videos and photos galleries from your cincopa account
 Author: Cincopa 
-Version: 1.130
+Version: 1.131
 */
 
 function _cpmp_plugin_ver()
 {
-	return 'wp1.130';
+	return 'wp1.131';
 }
 
 function _cpmp_afc()
